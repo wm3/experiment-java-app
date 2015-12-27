626028447213cb145053ea3fcf71fd8362b8c38c
Motoi Washida <wm3@app2.w3ch.jp>
2015-12-27 20:28:34 +0900

    add docker file

---
+++ container-base/circleci-load.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+NAME="$1"
+ARCHIVE="$HOME/docker/$NAME.tar"
+
+if [[ -e "$ARCHIVE" ]]
+then
+	docker load -i $ARCHIVE
+else
+	docker build -t $NAME container-base/$NAME
+	mkdir -p ~/docker
+	docker save $NAME > $ARCHIVE
+fi
